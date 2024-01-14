e5a8e29de2afd0e1794145769859488f2a7856c0
Corentin Rouff <62677123+Legion-in-hell@users.noreply.github.com>
2024-01-14 16:43:18 +0100

    Remove unused packages and Add Login / Signup pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { AuthProvider } from './AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import LoginPage from './components/LoginPage';
-import Dashboard from './components/Dashboard';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { AuthProvider } from "./AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+import SignUpPage from "./components/SignUpPage";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
@@ -22,11 +22,15 @@ function App() {
         <Router>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            } />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
             {/* Ajouter d'autres routes protégées ici */}
           </Routes>
         </Router>
--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,66 +1,71 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../AuthContext";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import axios from "axios";
 import { Link as RouterLink } from "react-router-dom";
-import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import { Container, Typography, TextField, Button, Link } from "@mui/material";
 
-function LoginPage() {
+function SignUpPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
-  const { login } = useAuth();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3001/api/login", {
+      // Remplacez par votre API d'inscription
+      await axios.post("http://localhost:3001/api/signup", {
         username,
+        email,
         password,
       });
-      login(response.data.token);
-      navigate("/");
+      navigate("/login"); // Redirige vers la page de connexion après l'inscription
     } catch (error) {
-      console.error("Erreur de connexion", error);
+      console.error("Erreur lors de l`'inscription", error);
     }
   };
 
   return (
     <Container component="main" maxWidth="xs">
       <Box
-        sx={{
-          marginTop: 8,
+        style={{
+          marginTop: "15%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
         <Typography component="h1" variant="h5">
-          Connexion
+          Inscription
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+        <form onSubmit={handleSubmit} style={{ width: "100%", marginTop: 1 }}>
           <TextField
+            variant="outlined"
             margin="normal"
             required
             fullWidth
             label="Nom d'utilisateur"
-            autoComplete="username"
             autoFocus
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
           <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
             margin="normal"
             required
             fullWidth
             label="Mot de passe"
             type="password"
-            autoComplete="current-password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
@@ -68,20 +73,21 @@ function LoginPage() {
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+            color="primary"
+            style={{ margin: "24px 0px 16px" }}
           >
-            Se Connecter
+            S'inscrire
           </Button>
-        </Box>
-        <Typography variant="body2" style={{ marginTop: "20px" }}>
-          Pas encore de compte?{" "}
-          <Link component={RouterLink} to="/signup">
-            Inscrivez-vous
-          </Link>
-        </Typography>
+          <Typography variant="body2" style={{ textAlign: "center" }}>
+            Déjà inscrit?{" "}
+            <Link component={RouterLink} to="/login">
+              Se connecter
+            </Link>
+          </Typography>
+        </form>
       </Box>
     </Container>
   );
 }
 
-export default LoginPage;
+export default SignUpPage;
