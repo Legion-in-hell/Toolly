1de2016a2172a4e3ad39c59a3af0b094944c9b5a
Corentin Rouff <62677123+Legion-in-hell@users.noreply.github.com>
2024-01-23 14:44:17 +0100

    Add FolderPage component for gestion of individual page folder

---
+++ src/components/FolderPage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import NavigationPanel from "./NavigationPanel";
+import TopBar from "./TopBar";
+import { Box } from "@mui/material";
+import PostItBoard from "./PostIt";
+
+const FolderPage = () => {
+  return (
+    <>
+      <TopBar />
+      <NavigationPanel />
+      <Box sx={{ display: "flex", marginLeft: "300px", marginTop: "50px" }}>
+        <div style={{ display: "flex", flex: 1, height: "100vh" }}>
+          <div
+            style={{
+              flex: 1,
+              paddingRight: "2px",
+              borderRight: "1px solid #ccc",
+            }}
+          >
+            Todo
+          </div>
+          <div
+            style={{
+              flex: 1,
+              paddingLeft: "2px",
+              justifyContent: "flex-end",
+              display: "flex",
+            }}
+          >
+            <PostItBoard />
+          </div>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default FolderPage;
